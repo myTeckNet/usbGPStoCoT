8f039798f9ecec7d5a1c97be798ccd04fcb59901
myTeckNet <joseph.glenn@mytecknet.com>
2025-06-21 17:45:31 -0700

    fixed typo

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="usbGPStoCot",
+    name="usbGPStoCoT",
     version="1.0.0",
     description="USB GPS CoT Transmitter for TAK Protocol v1",
     author="JR@myTeckNet",
     author_email="support@mytecknet.com",
     url="https://github.com/myTeckNet/usbGPStoCoT.git",
-    py_modules=["usbGPStoCot"],
+    py_modules=["usbGPStoCoT"],
     install_requires=[
         "pyserial",
         "pynmea2"
     ],
     entry_points={
         "console_scripts": [
-            "usbGPStoCot=usbGPStoCot:main"
+            "usbGPStoCoT=usbGPStoCoT:main"
         ]
     },
     classifiers=[
